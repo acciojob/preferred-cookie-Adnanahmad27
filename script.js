014dbe059035bed56ea10bb721ee26c67f87a6d3
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-03-10 19:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+let haveValue = false;
+localStorage.setItem('thing1','rahul');
+localStorage.getItem('thing1');
+localStorage.removeItem('thing1');
+
+form.addEventListener('submit' , (e)=>{
+	e.preventDefault();
+	let fontcolor = form.fontcolor.value;
+	let fontsize = form.fontsize.value;
+	localStorage.setItem('color',fontcolor);
+	localStorage.setItem('size',fontsize);
+	haveValue = true;
+});
+if(haveValue){
+	form.fontcolor.value = localStorage.getItem('color');
+	form.fontcsize.value = localStorage.getItem('size');
+}
